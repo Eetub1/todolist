cc852688c6eac778d551cc10097c760b549692b6
eeeeoino <eetu.e.oinonen@student.jyu.fi>
2025-06-26 12:31:12 +0300

    Commit before i break the whole thing

--- src/handleUI.js
+++ src/handleUI.js
@@ -1,18 +1,21 @@
+import { todos } from './todos.js'
+
 function drawProjects(projects) {
     const projectsCont = document.getElementById("projects")
 
     projects.projects.forEach(project => {
         const p = document.createElement("p")
-        p.addEventListener("click", drawProjectTodos)
+        p.id = project.project_id
+        p.addEventListener("click", (event) => drawProjectTodos(event))
         p.textContent = project.name
         projectsCont.appendChild(p)
-
     })
 }
 
-function drawProjectTodos() {
-    //painetaan p elementtiä. p:lle attribuutti project jolla arvo esim project="1" jos kuuluu projectille 1
+function drawProjectTodos(event) {
     //sitten etsitään todos taulukosta kaikki todot, joilla project_id on yhtäkuin 1
+    console.log(event.target);
+
 }
 
 export {drawProjects}
